8dbda62987e1a2c0de42c0caac782de7862dd1e4
Christian Steinmetz <csteinmetz1@gmail.com>
2017-11-15 15:45:05 -0500

    start of gating implementation

--- normalize/normalize.py
+++ normalize/normalize.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 
-
 def high_freq_shelving_filter(G, fc, fs, plot=False):
 
     Q = 1.0 / np.sqrt(2.0)
@@ -73,12 +72,41 @@ def high_pass_filter(fc, fs, plot=False):
 
     return b, a
 
-def loudness(audio, fs):
-    """ Compute the loudness for a signal."""
-    
-    # Perform "K" frequency weighting
+def apply_K_filter(audio, fs):
+
     b, a = high_freq_shelving_filter(4, 1680.0, fs)
     stage1 = signal.lfilter(b, a, audio)
 
     b, a = high_pass_filter(38.0, fs)
     stage2 = signal.lfilter(b, a, stage1)
+
+    return stage2
+
+def loudness(audio, fs):
+    """ Compute the loudness for a signal."""
+
+    if len(audio.shape) == 1: # for mono input standardize shape
+        audio = audio.reshape((audio.shape[0],1))
+
+    print audio
+
+    numSamples  = audio.shape[0] # length of input in samples
+    numChannels = audio.shape[1] # number of input channels
+
+    # "K" Frequency Weighting - account for the acoustic respose of the head and auditory system
+    for ch in range(numChannels):
+        audio[:,ch] = apply_K_filter(audio[:,ch], fs)
+
+    # Gating - ensures sections of silence or ambience do not skew the measurement
+
+    G = [1.0, 1.0, 1.0, 1.41, 1.41] # channel gains
+    T_g = 0.4 # 400 ms gating block
+    Gamma_a = -70.0 # -70 LKFS = absolute loudness threshold
+    overlap = 0.75 # overlap of 75% of the block duration
+    step = 1 - overlap
+
+    z = np.ndarray(shape=(numChannels,numSamples)) # tranpose input
+    T = numSamples / fs # length of the input in seconds
+    j_range = np.arange(0, int((T - T_g) / (T_g * step)))
+
+loudness(np.arange(4*48000).reshape(4*24000,2), 44100.0)
