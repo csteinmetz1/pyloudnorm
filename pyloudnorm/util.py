66f5d10839d7810ee17ec7b964fc632d84e61597
csteinmetz1 <csteinmetz1@gmail.com>
2020-04-02 21:11:40 +0200

    some small edits to error mesage within the valid_audio() method

--- pyloudnorm/util.py
+++ pyloudnorm/util.py
@@ -21,15 +21,15 @@ def valid_audio(data, rate, block_size):
         
     """
     if not isinstance(data, np.ndarray):
-        raise ValueError("Data must be of type numpy.ndarray")
+        raise ValueError("Data must be of type numpy.ndarray.")
     
     if not np.issubdtype(data.dtype, np.floating):
-        raise ValueError("Data must be floating point")
+        raise ValueError("Data must be floating point.")
 
     if data.ndim == 2 and data.shape[1] > 5:
-        raise ValueError("Audio must have five channels or less")
+        raise ValueError("Audio must have five channels or less.")
 
     if data.shape[0] < block_size * rate:
-        raise ValueError("Audio must be have length greater than the block size")
+        raise ValueError("Audio must have length greater than the block size.")
     
     return True
