ddc8880242960f351c0b3fd537d29849d83c81aa
csteinmetz1 <csteinmetz1@gmail.com>
2023-01-05 15:55:43 +0000

    updating setup and install workflow

--- setup.py
+++ setup.py
@@ -1,24 +1,41 @@
-from setuptools import setup
+from pathlib import Path
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+NAME = "pyloudnorm"
+DESCRIPTION = "Implementation of ITU-R BS.1770-4 loudness algorithm in Python"
+URL = "https://github.com/csteinmetz1/pyloudnorm"
+EMAIL = "c.j.steinmetz@qmul.ac.uk"
+AUTHOR = "Christian Steinmetz"
+REQUIRES_PYTHON = ">=3.0"
+VERSION = "0.1.1"
 
-setup(name='pyloudnorm',
-      version='0.1.0',
-      description='Implementation of ITU-R BS.1770-4 loudness algorithm in Python',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://github.com/csteinmetz1/pyloudnorm',
-      author='Christian Steinmetz',
-      author_email='cjstein@g.clemson.edu',
-      packages=['pyloudnorm'],
-      install_requires=['scipy>=1.0.1',
-                        'numpy>=1.14.2',
-                        'future>=0.16.0'],
-      classifiers=(
+HERE = Path(__file__).parent
+
+try:
+    with open(HERE / "README.md", encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=["pyloudnorm"],
+    install_requires=["scipy>=1.0.1", "numpy>=1.14.2", "future>=0.16.0"],
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-      )
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Scientific/Engineering",
+    ],
 )
